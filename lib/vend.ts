c3581efb5a156e36638f9eca37b606cbefc30b7e
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-10-10 17:03:34 -0700

    updated

--- lib/vend.ts
+++ lib/vend.ts
@@ -1,6 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
+
+export type VendResponse = {
+  data: any;
+  version: {
+    min: number;
+    max: number;
+  };
+};
 
 export const Vend = axios.create({
-  baseURL: ("https://" + process.env.VEND_API) as string,
+  baseURL: ('https://' + process.env.VEND_API) as string,
   headers: { Authorization: `Bearer ${process.env.VEND_API_KEY as string}` },
 });
--- lib/woocommerce.ts
+++ lib/woocommerce.ts
@@ -1,8 +1,8 @@
-import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
 
 export const WooCommerce = new WooCommerceRestApi({
-  url: ("https://" + process.env.WOOCOMMERCE_API) as string,
+  url: ('https://' + process.env.WOOCOMMERCE_API) as string,
   consumerKey: process.env.WOOCOMMERCE_CONSUMER_KEY as string,
   consumerSecret: process.env.WOOCOMMERCE_CONSUMER_SECRET as string,
-  version: "wc/v3",
+  version: 'wc/v3',
 });
