4777282a998422b7a7b6703e69b5a865e9c5311a
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-10-30 11:25:25 -0700

    added

---
+++ src/pages/api/products/inventory.ts
@@ -0,0 +1,25 @@
+import { Vend, VendResponse } from 'lib';
+import { ProductInventory } from 'models/product/types';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({
+      message: 'This method is not allowed on this route',
+    });
+  }
+
+  if (req.query && req.query.id) {
+    const id: string = req.query.id as string;
+
+    return res.json(
+      await Vend.get(`/products/${id}/inventory`)
+        .then((res) => res.data)
+        .then((vend: VendResponse<ProductInventory>) => vend.data),
+    );
+  } else {
+    return res.json({
+      message: 'No product ID was provided',
+    });
+  }
+};
