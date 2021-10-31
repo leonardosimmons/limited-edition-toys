8a585f07b3f1025a0c1e6265325deb48848ffe4f
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-10-30 20:12:38 -0700

    api 404 bug fix

--- models/product/queries.ts
+++ models/product/queries.ts
@@ -1,13 +1,8 @@
-import Axios from 'axios';
+import axios from 'axios';
 import { VendResponse } from 'lib';
 import { useQuery } from 'react-query';
 import { Default, Queries } from 'utils/keys';
 import { Product, ProductInventory, ProductPropertyOptions } from './types';
-
-// Base Options
-const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API as string,
-});
 
 //* -------------------------------------------------
 // Get All Products
@@ -17,13 +12,16 @@ export async function getAllProducts(
   pageSize?: number,
 ): Promise<VendResponse<Product[]>> {
   if (!version && !pageSize) {
-    return await axios.get(`/products`).then((res: any) => res.data);
+    return await axios
+      .get(process.env.NEXT_PUBLIC_PRODUCTS_API as string)
+      .then((res: any) => res.data);
   }
   return await axios
     .get(
-      `/products?version=${version || 0}&page_size=${
-        pageSize || Default.MAX_PRODUCTS_PER_QUERY
-      }`,
+      (process.env.NEXT_PUBLIC_PRODUCTS_API as string) +
+        `?version=${version || 0}&page_size=${
+          pageSize || Default.MAX_PRODUCTS_PER_QUERY
+        }`,
     )
     .then((res: any) => res.data);
 }
@@ -57,7 +55,9 @@ export function useProductTags() {
 export async function getProductById(
   id: string,
 ): Promise<VendResponse<Product>> {
-  return await axios.get(`/products/${id}`).then((res: any) => res.data);
+  return await axios
+    .get((process.env.NEXT_PUBLIC_PRODUCTS_API as string) + `/${id}`)
+    .then((res: any) => res.data);
 }
 
 export function useGetProductById(id: string) {
@@ -71,7 +71,9 @@ export async function getInventoryById(
   id: string,
 ): Promise<ProductInventory[]> {
   return await axios
-    .get(`/products/inventory?id=${id}`)
+    .get(
+      (process.env.NEXT_PUBLIC_PRODUCTS_API as string) + `/inventory?id=${id}`,
+    )
     .then((res: any) => res.data);
 }
 
