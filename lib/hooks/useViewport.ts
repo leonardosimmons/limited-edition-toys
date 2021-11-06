c7b29d720e74c5b5b375e8bd2d1b83a29506b9a0
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-11-05 23:40:42 -0700

    added tablet functionality

--- lib/hooks/useViewport.ts
+++ lib/hooks/useViewport.ts
@@ -1,23 +1,37 @@
 import React from 'react';
 import { useAppDispatch } from 'src/redux';
-import { setViewportType } from 'src/redux/models/ui';
 
-import { useTheme } from '@mui/material/styles';
-import { Breakpoint } from '@mui/material/styles';
+import { setViewportType } from 'src/redux/models/ui';
 import useMediaQuery from '@mui/material/useMediaQuery';
+
+import { Breakpoint, useTheme } from '@mui/material/styles';
 
 /**
  * [Material-ui]
  * Specifies the user's current viewport
  * @param breakpoint
  */
-export function useViewport(breakpoint?: number | Breakpoint) {
+export function useViewport(breakpoint?: string | Breakpoint) {
   const theme = useTheme();
   const dispatch = useAppDispatch();
-  const mobile = useMediaQuery(theme.breakpoints.down(breakpoint ?? 'md'));
+  const mobile = useMediaQuery(theme.breakpoints.down('tabletMd'));
+  const tablet = useMediaQuery(
+    theme.breakpoints.between('tabletMd', 'desktopSm'),
+  );
+  const desktop = useMediaQuery(theme.breakpoints.up('desktopSm'));
+  const custom = useMediaQuery(
+    typeof breakpoint === 'string'
+      ? breakpoint
+      : theme.breakpoints.up(breakpoint || 0),
+  );
 
   React.useEffect(() => {
     if (mobile) dispatch(setViewportType('mobile'));
-    else dispatch(setViewportType('desktop'));
-  }, [mobile]);
+    else if (tablet) dispatch(setViewportType('tablet'));
+    else if (desktop) dispatch(setViewportType('desktop'));
+  }, [mobile, tablet, desktop]);
+
+  return {
+    isValid: custom,
+  };
 }
