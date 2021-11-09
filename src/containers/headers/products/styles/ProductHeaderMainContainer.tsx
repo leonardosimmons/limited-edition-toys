e71a5b907be7f3f5020c52a4d90954ba0484f00a
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-11-09 11:35:02 -0800

    fixed responsiveness error

--- src/containers/headers/products/styles/ProductHeaderMainContainer.tsx
+++ src/containers/headers/products/styles/ProductHeaderMainContainer.tsx
@@ -7,28 +7,33 @@ import Container, { ContainerProps } from '@mui/material/Container';
 export const ProductHeaderMainContainer = styled(Container)<ContainerProps>(
   ({ theme }) => ({
     width: '100%',
-    height: '150px',
+    height: '200px',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: '20px',
     backgroundImage: `url(${Images.TOP_CLOUDS_LIGHT})`,
     backgroundSize: 'contain',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: '0 85%',
     [theme.breakpoints.up('mobileMd')]: {
-      height: '165px',
+      height: '200px',
       backgroundPosition: '0 100%',
     },
     [theme.breakpoints.up('mobileLg')]: {
       height: '195px',
+      marginTop: 0,
     },
-    [theme.breakpoints.up('mobileLg')]: {
+    [theme.breakpoints.up('tabletSm')]: {
       height: '225px',
     },
     [theme.breakpoints.up('tabletMd')]: {
       height: '275px',
       backgroundPosition: '0 120%',
       backgroundRepeat: 'repeat-x',
+    },
+    [theme.breakpoints.up('tabletLg')]: {
+      marginTop: '20px',
     },
     [theme.breakpoints.up('desktopSm')]: {
       height: '275px',
