025b8a1160dc957125adefb13dbea37a1e31e5f3
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-11-10 14:53:07 -0800

    temp

--- src/containers/sections/UpcommingEvents.tsx
+++ src/containers/sections/UpcommingEvents.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { SectionDivider, SectionTitle, SectionWrapper } from './styles/Section';
 
+import Container from '@mui/material/Container';
+import { useAppSelector } from 'src/redux';
+import { uiSelector } from 'src/redux/models/ui';
+
 type Props = {
   title: string;
 };
@@ -8,10 +12,26 @@ type Props = {
 const UpcommingEvents: React.FunctionComponent<Props> = ({
   title,
 }): JSX.Element => {
+  const ui = useAppSelector(uiSelector);
   return (
-    <SectionWrapper background maxWidth={false} sx={{ minHeight: '1100px' }}>
-      <SectionTitle variant="h2">{title}</SectionTitle>
+    <SectionWrapper
+      background
+      maxWidth={false}
+      sx={{
+        minHeight: `${
+          ui.status.viewport === 'mobile'
+            ? '600px'
+            : ui.status.viewport === 'tablet'
+            ? '1500px'
+            : '1100px'
+        }`,
+        justifyContent: 'center',
+      }}>
+      <SectionTitle variant="h2" sx={{ minHeight: 0 }}>
+        {title}
+      </SectionTitle>
       <SectionDivider variant="middle" />
+      <Container sx={{ width: '100%', height: '90%' }}>{''}</Container>
     </SectionWrapper>
   );
 };
