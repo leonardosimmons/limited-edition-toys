2c2f12821eba0363846892238f824b0722ea002f
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-11-15 16:22:02 -0800

    converted for generic styles

--- models/checkout/components/billing/BillingDetails.tsx
+++ models/checkout/components/billing/BillingDetails.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import data from 'data/pages/checkout.json';
 import { BillingState } from 'models/checkout/types';
+
 import { useCheckout } from 'models/checkout/useCheckout';
-import { BillingDetailsMainContainer } from './styles/BillingDetailsSection';
+
+import { CheckoutFormContainer } from '../styles/CheckoutStyles';
 
 import Typography from '@mui/material/Typography';
+
 import CheckoutInputGrid from '../CheckoutInputGrid';
 
 const BillingDetails: React.FunctionComponent = (): JSX.Element => {
@@ -22,14 +25,14 @@ const BillingDetails: React.FunctionComponent = (): JSX.Element => {
   // Render
 
   return (
-    <BillingDetailsMainContainer>
+    <CheckoutFormContainer>
       <Typography variant="h2">{data.billing.title}</Typography>
       <CheckoutInputGrid
         context={checkout.billing}
         config={data.billing.inputs}
         onChange={handleBillingChange}
       />
-    </BillingDetailsMainContainer>
+    </CheckoutFormContainer>
   );
 };
 
--- models/checkout/components/styles/CheckoutStyles.tsx
+++ models/checkout/components/styles/CheckoutStyles.tsx
@@ -1,7 +1,24 @@
 import { styled } from '@mui/material/styles';
 
 import Grid, { GridProps } from '@mui/material/Grid';
+import Container, { ContainerProps } from '@mui/material/Container';
 import FormControl, { FormControlProps } from '@mui/material/FormControl';
+
+export const CheckoutFormContainer = styled(Container)<ContainerProps>(
+  ({ theme }) => ({
+    width: '100%',
+    height: '100%',
+    ...theme.custom.centerColumn,
+    padding: '20px 0',
+    margin: '2rem 0',
+    '& h2.MuiTypography-h2': {
+      width: '100%',
+      margin: '20px 0 20px',
+      fontSize: '1.8rem',
+      fontWeight: 'bold',
+    },
+  }),
+);
 
 export const CheckoutInputMainGrid = styled(Grid)<GridProps>(({ theme }) => ({
   width: '100%',
