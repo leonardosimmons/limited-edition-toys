595d3084d2c4c9a689e9cb24c5f647b88b4e3ab9
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-11-16 01:55:43 -0800

    made responsive

--- src/containers/pages/styles/BillingPage.tsx
+++ src/containers/pages/styles/BillingPage.tsx
@@ -4,9 +4,13 @@ import Container, { ContainerProps } from '@mui/material/Container';
 
 export const BillingMainContainer = styled(Container)<ContainerProps>(
   ({ theme }) => ({
-    maxWidth: '1200px',
+    maxWidth: '1500px',
+    height: '100%',
     width: '100%',
     ...theme.custom.centerColumn,
+    [theme.breakpoints.up('tabletLg')]: {
+      flexDirection: 'row',
+    },
   }),
 );
 
@@ -15,6 +19,9 @@ export const BillingDetailsSection = styled(Container)<ContainerProps>(
     width: '100%',
     height: '100%',
     ...theme.custom.centerColumn,
+    [theme.breakpoints.up('tabletSm')]: {
+      flex: '0 1 50%',
+    },
   }),
 );
 
@@ -22,6 +29,20 @@ export const BillingSummarySection = styled(Container)<ContainerProps>(
   ({ theme }) => ({
     width: '100%',
     height: '100%',
-    backgroundColor: theme.palette.grey[400],
+    ...theme.custom.centerColumn,
+    alignSelf: 'flex-start',
+    [theme.breakpoints.up('tabletSm')]: {
+      flex: '0 1 50%',
+    },
+    '& table.MuiTable-root': {
+      border: `2px solid ${theme.palette.grey[300]}`,
+    },
+    '& div.MuiContainer-root > div.MuiContainer-root': {
+      border: `2px solid ${theme.palette.grey[300]}`,
+      borderTop: 0,
+      '& div.MuiBox-root': {
+        marginBottom: 0,
+      },
+    },
   }),
 );
